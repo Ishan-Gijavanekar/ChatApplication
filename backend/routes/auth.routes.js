59c7a3a259af73dc9212d9629851d1e7bcfc2bf6
Ishan-Gijavanekar <isg.sachin@gmail.com>
2024-12-08 23:18:26 +0530

    Complete user model and controller and routes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { signin, signout, signup } from '../controllers/auth.controllers.js'
+import { checkAuth, signin, signout, signup, upadteProfile } from '../controllers/auth.controllers.js'
+import { protectedRoute } from '../middlewares/auth.middleware.js'
 
 const router = express.Router()
 
@@ -8,5 +9,9 @@ router.post("/signup", signup)
 router.post("/signin", signin)
 
 router.post("/signout", signout)
+
+router.put("/update-profile", protectedRoute ,upadteProfile)
+
+router.post("/check", protectedRoute, checkAuth)
 
 export default router
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,22 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
+import cookieParser from 'cookie-parser'
+
 const app = express()
 
 const port = process.env.PORT;
 
+
+app.use(express.json())
+app.use(cookieParser());
+
 import authRoutes from '../routes/auth.routes.js'
+import messageRoutes from '../routes/messages.routes.js'
 import { connectDb } from '../lib/db.js';
 
 app.use('/api/auth', authRoutes)
+app.use('/api/messages', messageRoutes)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)

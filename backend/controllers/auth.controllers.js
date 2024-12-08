2aa74e72f3c3a4e9825fc7aaf39312b763714b6d
Ishan-Gijavanekar <isg.sachin@gmail.com>
2024-12-08 21:09:44 +0530

    Initial Commit

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    res.send("Signup")
+}
+
+export const signin =  (req, res) => {
+    res.send("Signin")
+}
+
+export const signout = (req, res) => {
+    res.send("Signout")
+}

8f5c4c062c6bacc3205376053f338ec2a599e6b2
Ishan-Gijavanekar <isg.sachin@gmail.com>
2024-12-11 10:23:17 +0530

    Home Page Complete

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import toast from 'react-hot-toast'
 import {create} from 'zustand'
 import { axiosInstance } from '../lib/axios'
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
     messages: [],
     users: [],
     selectedUser: null,
@@ -39,7 +39,14 @@ export const useChatStore = create((set) => ({
     },
 
     sendMessage: async (data) => {
-        
+        const {selectedUser, messages} = get()
+        try {
+            const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, data)
+            set({messages:[...messages,res.data]})
+        } catch (error) {
+            console.log("Error in sendMessage: ", error)
+            toast.error(error.response.data.message)
+        }
     },
 
     setSelectedUser: (selectedUser) => set({selectedUser}),

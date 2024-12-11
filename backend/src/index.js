009740bd5addeb3a16371b393ac26cc09ff0b30a
Ishan-Gijavanekar <isg.sachin@gmail.com>
2024-12-11 20:07:54 +0530

    last minute changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,10 +27,10 @@ app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/public")));
+    app.use(express.static(path.join(__dirname, "../frontend")));
 
     app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/public", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "index.html"));
 });
 
 }
